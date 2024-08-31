c889905e4b0d678d1a416ab7df1f152b7b0d6a98
maniraj <manirajmca.ac@gmail.com>
2024-08-31 11:55:42 +0530

    Footer component pagination update

--- src/app/layout/app.footer.component.ts
+++ src/app/layout/app.footer.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-import { LayoutService } from "./service/app.layout.service";
+import { LayoutService } from './service/app.layout.service';
+import { AnyCatcher } from 'rxjs/internal/AnyCatcher';
 
 @Component({
     selector: 'app-footer',
-    templateUrl: './app.footer.component.html'
+    templateUrl: './app.footer.component.html',
+    styleUrls: ['./app.footer.component.scss'],
 })
 export class AppFooterComponent {
-    constructor(public layoutService: LayoutService) { }
+    first: number = 10;
+    rows: number = 3;
+    constructor(public layoutService: LayoutService) {}
+    onPageChange(event: any) {}
 }
